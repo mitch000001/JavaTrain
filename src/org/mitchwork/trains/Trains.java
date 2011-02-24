a7890e0d3b20e2dfd3cfc2153aaef8741487cc54
mitch000001 <mitch.wagna@gmail.com>
2011-02-24 22:11:53 +0100

    removed routes of the project

--- src/org/mitchwork/trains/Trains.java
+++ src/org/mitchwork/trains/Trains.java
@@ -2,53 +2,48 @@ package org.mitchwork.trains;
 
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Vector;
 
 public class Trains {
 	
 	// Given cities
-	public static City c1 = new City("A");
-	public static City c2 = new City("B");
-	public static City c3 = new City("C");
-	public static City c4 = new City("D");
-	public static City c5 = new City("E");
+	private static final City c1 = new City("A");
+	private static final City c2 = new City("B");
+	private static final City c3 = new City("C");
+	private static final City c4 = new City("D");
+	private static final City c5 = new City("E");
 	
-	// Given routes/graphs
-	public static Route r1 = new Route("AB5");
-	public static Route r2 = new Route("BC4");
-	public static Route r3 = new Route("CD8");
-	public static Route r4 = new Route("DC8");
-	public static Route r5 = new Route("DE6");
-	public static Route r6 = new Route("AD5");
-	public static Route r7 = new Route("CE2");
-	public static Route r8 = new Route("EB3");
-	public static Route r9 = new Route("AE7");
+	// Given graphs
+	private static final Trip t1 = new Trip("AB",5);
+	private static final Trip t2 = new Trip("BC",4);
+	private static final Trip t3 = new Trip("CD",8);
+	private static final Trip t4 = new Trip("DC",8);
+	private static final Trip t5 = new Trip("DE",6);
+	private static final Trip t6 = new Trip("AD",5);
+	private static final Trip t7 = new Trip("CE",2);
+	private static final Trip t8 = new Trip("EB",3);
+	private static final Trip t9 = new Trip("AE",7);
 	
 	// Given Trips
-	public static Trip t1 = new Trip("A-B-C",9);
-	public static Trip t2 = new Trip("A-D-C",13);
-	public static Trip t3 = new Trip("A-E-B-C-D",22);
-	public static Trip t4 = new Trip("A-D",5);
+	private static final Trip t10 = new Trip("A-B-C",9);
+	private static final Trip t11 = new Trip("A-D-C",13);
+	private static final Trip t12 = new Trip("A-E-B-C-D",22);
 	
-	// Given Cities, Routes and trips in HashMaps
-	public static HashMap<String, City> cities = new HashMap<String, City>();
-	public static HashMap<String, Route> routes = new HashMap<String, Route>();
-	public static HashMap<String, Trip> trips = new HashMap<String, Trip>();
-	
-	public static Vector<Trip> uniquetrips = new Vector<Trip>();
-	public static Scanner scanner = new Scanner(System.in);
+	// Given Cities, Trips and trips in HashMaps
+	private static final HashMap<String, City> cities = new HashMap<String, City>();
+	private static final HashMap<String, Trip> trips = new HashMap<String, Trip>();
+
+    private static final Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		populateCities();
-		populateRoutes();
-		populateRoutes();
+		populateTrips();
 		System.out.println("Welcome, User.");
 		programMenu();
 	}
 	
 	private static void programMenu() {
 		System.out.println("To enter a graph press 'g', to enter a trip, press 't'.");
-		System.out.println("If you want to calculate route-specific details such distance, number of trips or shortest route, press 'c'.");
+		System.out.println("If you want to calculate Trip-specific details such distance, number of trips or shortest Trip, press 'c'.");
 		System.out.println("If you want to quit the program, press 'q'.");
 		System.out.println("Please enter your choice (g/t/c/q):");
 		String input = scanner.next();
@@ -63,7 +58,7 @@ public class Trains {
 	private static void validateMenu(char c) {
 		switch (c) {
 		case 'g': {
-			enterRoute(); 
+			enterGraph();
 			break;
 		}
 		case 't': {
@@ -83,7 +78,7 @@ public class Trains {
 			break;
 		}
 		default: {
-			System.out.println("You have entered an unexpected Charakter!");
+			System.out.println("You have entered an unexpected Character!");
 			programMenu(); 
 			break;
 		}
@@ -100,47 +95,39 @@ public class Trains {
 		System.out.print("Please enter a trip to test its existence and to calculate the distance. ");
 		System.out.println("The convention of input is 'letter'-'letter'-'letter..., e.g. A-B-C:");
 		String input = scanner.next();
-		while (!input.matches("{\\p{Alpha}-}*\\p{Alpha}")){
-			System.out.println("You have entered a sequence that is not supported. Please try again:");
-			input = scanner.next();
-		}
-		if (tripExists(input.toUpperCase())) {
+		Trip t = new Trip(input);
+		if (trips.containsKey(t.getName())) {
 			System.out.println(trips.get(input).getDistance());
 		} else { 
 			System.out.println("NO SUCH ROUTE");
 		}
 		System.out.println("Do you want to enter another trip? (y/n)");
 		input = scanner.next();
-		if (input.toLowerCase()=="y") {
-			enterRoute();
+		if (input.toLowerCase().contains("y")) {
+			enterTrip();
 		} else  {
 			System.out.println("Returning to main menu...");
 			programMenu();
 		}
 	}
 
-	private static void enterRoute() {
+	private static void enterGraph() {
 		System.out.println("Please enter a graph to test its existence. The convention of input is 'letter''letter''digit', e.g. 'AB5':");
 		String input = scanner.next();
-		while (input.length()<3) {
+		while (input.length()>3) {
 			System.out.println("You have entered too much Characters. The input is limited to 3 Characters.");
 			System.out.println("Please try again:");
 			input = scanner.next();
 		}
-		while (!input.matches("\\p{Alpha}\\p{Alpha}\\d")) {
-			System.out.println("You have entered a Sequence that isn't supported. The convention of input is 'letter''letter''digit', e.g. 'AB5'.");
-			System.out.println("Please try again:");
-			input = scanner.next();
-		}
-		if (routeExists(input.toUpperCase())) {
+		if (trips.containsKey(input.substring(0, 2).toUpperCase())) {
 			System.out.print("The entered graph exists. ");
 		} else {
 			System.out.println("The graph does not exist. ");
 		}
 		System.out.println("Do you want to enter another graph? (y/n)");
 		input = scanner.next();
-		if (input.toLowerCase().charAt(0)=='y') {
-			enterRoute();
+		if (input.toLowerCase().contains("y")) {
+			enterTrip();
 		} else  {
 			System.out.println("Returning to main menu...");
 			programMenu();
@@ -155,70 +142,33 @@ public class Trains {
 		cities.put(c5.getShortCode(), c5);
 	}
 
-	private static void populateRoutes() {
-		routes.put(r1.toString(), r1);
-		routes.put(r2.toString(), r2);
-		routes.put(r3.toString(), r3);
-		routes.put(r4.toString(), r4);
-		routes.put(r5.toString(), r5);
-		routes.put(r6.toString(), r6);
-		routes.put(r7.toString(), r7);
-		routes.put(r8.toString(), r8);
-		routes.put(r9.toString(), r9);
-	}
-	
-	private static boolean routeExists(Route r) {
-		if (routes.containsValue(r)) {
-			return true;
-		} else
-			return false;
-	}
-	
-	private static boolean routeExists(String s) {
-		if (routes.containsKey(s)) {
-			return true;
-		} else
-			return false;
-	}
-	
-	private static boolean tripExists(String s) {
-		if (trips.containsKey(s)) {
-			return true;
-		} else
-			return false;
-	}
-	
-	private boolean isConnectedTo(City a, City b) {
-		for (String key : cities.keySet())
-			if (key.endsWith(b.getShortCode()))
-				return true;
-		return false;
-	}
-	
-	private void populateTrips() {
+	private static void populateTrips() {
 		trips.put(t1.toString(), t1);
 		trips.put(t2.toString(), t2);
 		trips.put(t3.toString(), t3);
 		trips.put(t4.toString(), t4);
+		trips.put(t5.toString(), t5);
+		trips.put(t6.toString(), t6);
+		trips.put(t7.toString(), t7);
+		trips.put(t8.toString(), t8);
+		trips.put(t9.toString(), t9);
+        trips.put(t10.toString(), t10);
+        trips.put(t11.toString(), t11);
+        trips.put(t12.toString(), t12);
 	}
-	
-	private boolean hasRouteTo(City a, City b) {
+
+	private boolean isConnectedTo(City a, City b) {
+		String connection = (a.getShortCode() + b.getShortCode());
+        for (String key : trips.keySet()) {
+            if (key.equals(connection))
+                return true;
+        }
 		return false;
 	}
 	
-	private Vector<Route> routesFrom(City a) {
-		Vector<Route> routesFrom = new Vector<Route>();
-		for (String key : routes.keySet()) {
-			if (key.startsWith(a.getShortCode())) {
-				routesFrom.add(routes.get(key));
-			}
-		}
-		return routesFrom;
-	}
-
 	private Trip getShortestTrip(City startCity, City endCity) {
-		// TODO Create method
+
 		return null;
-	}
+    }
 	
 }
